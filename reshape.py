a69188a9645daad41d671724406ef56fbd98792a
Jonatas Alves <alves.jnts@gmail.com>
2019-06-30 03:30:13 -0300

    Alterado modo de entrada da resolução da imagem original

--- reshape.py
+++ reshape.py
@@ -12,14 +12,15 @@ def divide_image(arr, nrows, ncols):
                .reshape(-1, nrows, ncols))
 
 
-def rebuild_image(arr, h, w):
+def rebuild_image(arr, original_image_shape):
     """
-    Return an array of shape (h, w) where
+    Return an array of shape (h, w) = original_image_shape where
     h * w = arr.size
 
     If arr is of shape (n, nrows, ncols), n sublocks of shape (nrows, ncols),
     then the returned array preserves the "physical" layout of the sublocks.
     """
+    h, w = original_image_shape
     n, nrows, ncols = arr.shape
     return (arr.reshape(h//nrows, -1, nrows, ncols)
                .swapaxes(1, 2)
